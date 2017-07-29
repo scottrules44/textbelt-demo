468168a8b796dd73fedebd9387368d37ca243c50
scottrules44 <scottrules44@gmail.com>
2017-07-28 19:19:54 -0500

    Bug fixes
    Bug fixes

--- main.lua
+++ main.lua
@@ -16,44 +16,6 @@ icon.x, icon.y = 30-scale0, scale0Y+ 40
 
 local title = display.newText( "Textbelt Plugin", display.contentCenterX,  scale0Y+ 40, native.systemFont, 30 )
 
---radio buttons
-local textRadio = display.newGroup()
-
--- Create two associated radio buttons (inserted into the same display group)
-local textButton1 = widget.newSwitch(
-    {
-        x = display.contentCenterX-100,y= display.contentCenterY+30,
-        style = "radio",
-        id = "textButton1",
-        initialSwitchState = true,
-    }
-)
-textRadio:insert( textButton1 )
-local textButtonText = display.newText( "Send to US", textButton1.x+30,textButton1.y, native.systemFontBold,  20)
-textButtonText.anchorX = 0
-
-local textButton2 = widget.newSwitch(
-    {
-        x = display.contentCenterX-100,y= display.contentCenterY+80,
-        style = "radio",
-        id = "textButton2",
-    }
-)
-textRadio:insert( textButton2 )
-local textButtonText2 = display.newText( "Send to Canada", textButton2.x+30,textButton2.y, native.systemFontBold,  20)
-textButtonText2.anchorX = 0
-
-local textButton3 = widget.newSwitch(
-    {
-        x = display.contentCenterX-100,y= display.contentCenterY+130,
-        style = "radio",
-        id = "textButton3",
-    }
-)
-textRadio:insert( textButton3 )
-local textButtonText3 = display.newText( "Send to International", textButton3.x+30,textButton3.y, native.systemFontBold,  20)
-textButtonText3.anchorX = 0
-
 --number
 local numberText = display.newText( "Phone Number(no dashes)", display.contentCenterX, display.contentCenterY-130, native.systemFont, 18 )
 local numberTextBox = native.newTextField( display.contentCenterX, display.contentCenterY-100, display.actualContentWidth, 30 )
@@ -91,26 +53,9 @@ rect:addEventListener( "touch", function ( e )
         return true
     end
     if (e.phase == "began") then
-        shouldSend = 1
         rect:setFillColor( .5 )
     elseif (e.phase == "ended") then
-        if (shouldSend == 1) then
-            print( numberTextBox.text, messageTextBox.text )
-            if (textButton1.isOn) then -- send international
-                textbelt.sendUS(numberTextBox.text, messageTextBox.text, textLis)
-            end
-            if (textButton2.isOn) then -- send international
-               textbelt.sendCanada(numberTextBox.text, messageTextBox.text, textLis) 
-            end
-            if (textButton3.isOn) then -- send international
-               textbelt.sendEverywhere(numberTextBox.text, messageText.text, textLis)  
-            end
-            turnOffTouch = true
-        else
-            rect:setFillColor( 1 )
-        end
-        shouldSend = 0
-    else
+        textbelt.sendMessage(numberTextBox.text, messageTextBox.text, textLis)
         rect:setFillColor( 1 )
         shouldSend = 0
     end
